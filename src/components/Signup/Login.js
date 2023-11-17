fd9b8882950882e883dd09466c979627c92f198c
Shubham Chauhan <shubham.chauhan826@gmail.com>
2023-11-17 13:17:22 +0530

    Sign-In & Sign-Up page added

--- src/components/Signup/Login.js
+++ src/components/Signup/Login.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div>
       <div>
         <Header />
       </div>
-      <div>
+      <div className="absolute">
         <img
           alt="bg-img"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/a09bb938-2d90-42ae-986e-5a3e4abf9e77/8eb1e781-3494-4aa4-9405-268ca6473e4c/IN-en-20231113-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         ></img>
+      </div>
+      <div>
+        <form className="absolute bg-black px-12 mt-52 ml-[36rem] grid rounded-2xl bg-opacity-80">
+          <h1 className="text-white text-2xl py-5">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </h1>
+          <input
+            type="text"
+            placeholder="Email"
+            className="p-4 rounded-xl w-80"
+          ></input>
+          {!isSignIn && <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 rounded-xl mt-2 w-80"
+          ></input>}
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-4 rounded-xl mt-2 w-80"
+          ></input>
+          <button className="bg-red-700 rounded-md my-8 h-12 text-white">
+            {isSignIn ? "Sign-In" : "Sign-Up"}
+          </button>
+
+          <div>
+            <h2
+              className="p-2 my-4 cursor-pointer text-white"
+              onClick={toggleSignIn}
+            >
+              {isSignIn ? "New to Netflix? Sign Up Now." : "Already a User? Login now."}
+            </h2>
+          </div>
+        </form>
       </div>
     </div>
   );
